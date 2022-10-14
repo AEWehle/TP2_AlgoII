2b117ac75bccec53107fdd544554256d4d950b7e
AEWehle <awehle@fi.uba.ar>
2022-10-13 15:09:44 -1200

    Este es un commit donde cree el main.cpp y modifiqué el .gitignore

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+#include <string>
+#include <fstream> 
+using namespace std; 
+
+
+//./TP2 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
+// COMANDO: g++ main.cpp -o TP2 -Wall -Werror -Wconversion
+// valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./TP1
+// gdb ./Prueba
+
+// git status        Para ver lo que esta para subirse
+// git add .         Para agregar a las cosas a subirse
+// git commit -m "mensaje para nuestro grupo"       Para guardarlo y en un futuro subirlo con git push
+// git log --oneline        Para ver el historial de Commits
+
+
+// Para comentar automaticamente pueden usar ctrl + ç
+
+
+/*_____________________________TP2: LA RESERVA SOÑADA____________________*/
+int main(){
+
+    cout << "Hola,\nEn este programa te aseguramos una Reserva Soñada.\n     DISFRUTALO!" << endl;
+    
+    // Biblioteca* mi_biblioteca  =  leer_archivo();
+    // int eleccion = 0;
+
+    // while ( mi_biblioteca -> lista_de_libros != nullptr ){
+    //     eleccion = pedir_eleccion();
+    //     cout << "Elegiste la opcion "<< eleccion << endl << endl;
+    //     ejecutar_eleccion( mi_biblioteca, eleccion );
+    // }
+    
+    // delete mi_biblioteca;
+    // mi_biblioteca = nullptr;
+    return 0;
+}
