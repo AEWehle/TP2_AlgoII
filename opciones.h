69e63e0867db0837efda87277e8bfdc11a553695
lnimo28 <115752347+lnimo28@users.noreply.github.com>
2022-11-02 20:39:27 -0300

    revisión pre y pos

--- opciones.h
+++ opciones.h
@@ -4,26 +4,19 @@
 #include "funciones_main.h"
 using namespace std; 
 
-/* ________________________opciones.h_____________________________________
-  En este .h se encuentran declaradas las funciones que se ejecutan según la 
-elección del usuario.
-_______________________________________________________________________________*/
-
 
 /*________listar_animales()______________________________________________________
- PRE:  El usuario pidio listar los animales, recibe una structura tipo Guarderia
- con la informacion actual.
- POST: Imprime en terminal los datos de los animales guardados.
+ PRE:  Recibe puntero a una Guarderia (contiene una lista de animales).
+ POST: Imprime por la terminal la lista de animales contenidos en mi_guarderia.
 _______________________________________________________________________________*/
 void listar_animales( Guarderia* mi_guarderia );
 
 
 /*________rescatar_animal()______________________________________________________
- PRE:  Serescató un animal y hay que agregarlo a la lista de animales en la rese-
- rva recibe una structura tipo Guarderia con la informacion actual.
- POST: Guarda el animal.
+ PRE:  Recibe puntero a una Guarderia (contiene una lista de animales).
+ POST: Guarda el animal rescatado en la lista.
 _______________________________________________________________________________*/
-void rescatar_animal      ( Guarderia* mi_guarderia );
+void rescatar_animal( Guarderia* mi_guarderia );
 
 
 /*________buscar_animal()______________________________________________________
@@ -31,26 +24,27 @@ void rescatar_animal      ( Guarderia* mi_guarderia );
  POST: Le pregunta al usuario el nombre del animal, si lo encunetra lo muestra,
   sino se lo notifica.
 _______________________________________________________________________________*/
-void buscar_animal        ( Guarderia* mi_guarderia );
+void buscar_animal( Guarderia* mi_guarderia );
  
 
  /*________cuidar_animal()______________________________________________________
- PRE:  
- POST: 
+ PRE:  Recibe puntero a una Guarderia (contiene una lista de animales).
+ POST: Muestra menú con opciones para cuidar al animal y le pide al usuario que
+      elija uno.
 _______________________________________________________________________________*/
-void cuidar_animales      ( Guarderia* mi_guarderia );
+void cuidar_animales( Guarderia* mi_guarderia );
  
 
  /*________adoptar_animal()______________________________________________________
- PRE:  Recibe la base de datos actual.
- POST: Le pregunta al usuario qué tamaño dispone y le muetra un lista con los 
+ PRE:  Recibe puntero a una Guarderia (contiene una lista de animales).
+ POST: Le pregunta al usuario qué tamaño dispone y le muestra un lista con los 
  animales disponibles. Puede cancelar o adoptar ingresando el nombre.
 _______________________________________________________________________________*/
-void adoptar_animal       ( Guarderia* mi_guarderia );
+void adoptar_animal( Guarderia* mi_guarderia );
 
 
 /*________guardar_salir()______________________________________________________
- PRE:  Recibe la base de datos actual.
+ PRE:  Recibe puntero a una Guarderia (contiene una lista de animales).
  POST: Guarda en el archivo llamado RUTA_ARCHIVO la informacion en la base de
   datos y libera la memoria.
 _______________________________________________________________________________*/
@@ -67,8 +61,6 @@ const Manejo_Guarderia funcion_elegida[ CANTIDAD_OPCIONES ] = {
     adoptar_animal,
     guardar_salir,
 };
-
-
 
 
 #endif
