229f7fd16913b60642e978ab3235a47d85d51259
lnimo28 <115752347+lnimo28@users.noreply.github.com>
2022-11-03 01:39:17 -0300

    actualizado

--- Archivos_auxiliares/lista.h
+++ Archivos_auxiliares/lista.h
@@ -96,6 +96,8 @@ Dato* Lista<Dato>::consulta(int pos) {
 template <typename Dato>
 void Lista<Dato>::baja(int pos) {
     Nodo<Dato>* a_borrar = this -> primero;
+    cout << "CANTIDAD ACAAAAA " << cantidad << endl;
+    cout << "POSICION " << pos << endl;
     if (pos == 1) {
         this -> primero = a_borrar->obtener_siguiente();
     }
@@ -103,10 +105,10 @@ void Lista<Dato>::baja(int pos) {
         Nodo<Dato>* anterior = obtener_nodo(pos - 1);
         a_borrar = anterior->obtener_siguiente();
         anterior->cambiar_siguiente(a_borrar->obtener_siguiente());
+        
     }
-    //delete[] a_borrar;
-    delete a_borrar->obtener_dato(); // Ojo si dentro de los nodos pedimos memoria
-    delete a_borrar;
+    delete a_borrar->obtener_dato();
+    delete a_borrar; // Ojo si dentro de los nodos pedimos memoria
     this -> cantidad--;
 }
 
@@ -117,9 +119,9 @@ bool Lista<Dato>::vacia() {
 
 template <typename Dato>
 Lista<Dato>::~Lista() {
-    //while (! vacia()){
-   //     baja(1);
-    //}
+    while (! vacia()){
+        baja(1);
+    }
 }
 
 template <typename Dato>
